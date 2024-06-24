c7a2517fc99dd17c4f4480c56ae7d65bc1504950
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-06-24 11:50:32 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Abdulmajid"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toLocaleUpperCase()
+let shortGreeting = "Welcome, " + currentUser[0] + " !"
